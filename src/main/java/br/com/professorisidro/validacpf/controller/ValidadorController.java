fc767353925fd0b2944728beede8b640f5eac0ff
danie <daniel.freitas.ms1@gmail.com>
2020-08-05 15:19:22 -0300

    bem-vindo api

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -13,10 +13,20 @@ public class ValidadorController {
 	
 	@GetMapping("/validacpf")
 	public String validaCPF(@RequestParam (required = false) String cpf) {
+
+		if(cpf == "" || cpf == " "){
+			return "{\"status\": \"conteudo_vazio\"}";
+		}
+
 		if (Validador.isCPF(cpf)) {
 			return "{\"status\": \"valido\"}";
 		}
-		 return "{\"status\": \"invalido\"}";
+		 	return "{\"status\": \"invalido\"}";
+	}
+
+	@GetMapping("/")
+	public String index (){
+		return "{\"status\": \"bem-vindo a API\"}";
 	}
 
 }
